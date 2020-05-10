a78d75202ab3c2a20a6b1aa6d390c060728e26f3
Sebas <sebas.warsaw@gmail.com>
2020-05-10 20:54:33 -0300

    Change list

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,20 +1,8 @@
-import React from 'react';
-import { List } from 'semantic-ui-react';
+import React from "react";
+import { List } from "semantic-ui-react";
 
 const Features = () => {
-  const list = [
-    'React 16',
-    'React Router 4',
-    'Semantic UI as the CSS Framework',
-    'Hot Module Replacement',
-    'CSS Autoprefixer',
-    'CSS Modules with SourceMap',
-    'Stage 1 Preset',
-    'Webpack 4',
-    'Code splitting by Route and Vendor',
-    '<a href="https://github.com/th0r/webpack-bundle-analyzer" target="_blank">Webpack Bundle Analyzer</a>',
-    "Take a look at <a href='https://github.com/esausilva/react-starter-boilerplate-hmr/blob/master/package.json' target='_blank'>package.json</a>"
-  ];
+  const list = ["Dogs", "Cats", "Elms", "Lions"];
 
   return (
     <List>
