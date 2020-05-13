be503bc9640d9812e2289a3e131e7391e46b2564
Sebas <sebas.warsaw@gmail.com>
2020-05-13 09:11:35 -0300

    file loader

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -1,49 +1,60 @@
-const commonPaths = require('./common-paths');
+const commonPaths = require("./common-paths");
 
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
   entry: {
-    vendor: ['semantic-ui-react']
+    vendor: ["semantic-ui-react"],
   },
   output: {
     path: commonPaths.outputPath,
-    publicPath: '/'
+    publicPath: "/",
   },
   module: {
     rules: [
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+    ],
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         styles: {
-          name: 'styles',
+          name: "styles",
           test: /\.css$/,
-          chunks: 'all',
-          enforce: true
+          chunks: "all",
+          enforce: true,
         },
         vendor: {
-          chunks: 'initial',
-          test: 'vendor',
-          name: 'vendor',
-          enforce: true
-        }
-      }
-    }
+          chunks: "initial",
+          test: "vendor",
+          name: "vendor",
+          enforce: true,
+        },
+      },
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: `public/index.html`,
-      favicon: `public/favicon.ico`
-    })
-  ]
+      favicon: `public/favicon.ico`,
+    }),
+  ],
 };
 
 module.exports = config;
