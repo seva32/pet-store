30fdbb20347cf1faf37073e8822727971b1b4b91
Sebas <sebas.warsaw@gmail.com>
2020-05-16 11:15:01 -0300

    styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
-//import importedComponent from "react-imported-component";
-//import "react-imported-component/macro";
-import { imported, lazy, useImported } from "react-imported-component/macro";
+import importedComponent from "react-imported-component";
+import "react-imported-component/macro";
 
 import Home from "./Home";
 import Loading from "./Loading";
 
-const AsyncDynamicPAge = imported(
+const AsyncDynamicPAge = importedComponent(
   () => import(/* webpackChunkName:'DynamicPage' */ "./DynamicPage"),
   {
     LoadingComponent: Loading,
   }
 );
-const AsyncNoMatch = imported(
+const AsyncNoMatch = importedComponent(
   () => import(/* webpackChunkName:'NoMatch' */ "./NoMatch"),
   {
     LoadingComponent: Loading,
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Header, Container, Divider, Icon } from "semantic-ui-react";
 
-import { pullRight, h1 } from "./layout.css";
+import { pullRight, h1, containerCustom } from "./layout.css";
 
 const Layout = ({ children }) => {
   const [name, setName] = useState("react-webpack");
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
     setName("react-boilerplate-hmr");
   }
   return (
-    <Container>
+    <Container className={containerCustom}>
       <Link to="/">
         <Header as="h1" className={h1}>
           {name}
