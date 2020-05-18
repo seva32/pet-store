4bb949492d55360d06558979925a4220729aaa90
Sebas <sebas.warsaw@gmail.com>
2020-05-18 08:42:36 -0300

    link home

--- src/components/DynamicPage.js
+++ src/components/DynamicPage.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Header } from 'semantic-ui-react';
+import React from "react";
+import { Header } from "semantic-ui-react";
 
-import Layout from './Layout';
+import Layout from "./Layout";
 
 const DynamicPage = () => {
   return (
-    <Layout>
+    <Layout name={"<< Pet Store"}>
       <Header as="h2">Dynamic Page</Header>
       <p>This page was loaded asynchronously!!!</p>
     </Layout>
