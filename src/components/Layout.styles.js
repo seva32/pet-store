939ca6214ed696c9982cbffd363cad3e34f1cb0e
Sebas <sebas.warsaw@gmail.com>
2020-05-21 10:02:45 -0300

    home background

--- src/components/Layout.styles.js
+++ src/components/Layout.styles.js
@@ -2,5 +2,5 @@ import styled from "styled-components";
 import { Container } from "semantic-ui-react";
 
 export const StyledContainer = styled(Container)`
-  background-color: green;
+  padding: 1rem 0;
 `;
